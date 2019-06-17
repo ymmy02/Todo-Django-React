a05f6ee33a6bf992ef53805d1ac6179d6b97b050
Kohei Yamamoto <kh2yam1@gmial.com>
2019-06-17 11:30:17 -0400

    Update frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+  {
+    'id': 1,
+    'title': '1st Item',
+    'description': 'Description here.'
+  },
+  {
+    'id': 2,
+    'title': '2nd Item',
+    'description': 'Another description here.'
+  },
+  {
+    'id': 3,
+    'title': '3rd Item',
+    'description': 'Third description here.'
+  }
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      todos: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:8000/api/')
+        .then(res => {
+          this.setState({ todos: res.data });
+        })
+        .catch(err => { console.log(err); });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.todos.map(item => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <span>{item.description}</span>
+          </div>  
+        ))}
+      </div>
+    );
+  }
+
 }
 
 export default App;
